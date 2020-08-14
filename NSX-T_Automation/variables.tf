de5e2f7937f906dfa9add8090ab168efea600235
assafsauer <as.sauer@gmail.com>
2020-08-14 21:09:37 +0200

    Create variables.tf

---
+++ NSX-T_Automation/variables.tf
@@ -0,0 +1,14 @@
+# Variables
+ 
+variable "nsx_manager" {
+    default = "192.168.1.70"
+}
+ 
+# Username & Password for NSX-T Manager
+variable "username" {
+  default = "admin"
+}
+ 
+variable "password" {
+    default = "SAuer1357N@1357N"
+}
